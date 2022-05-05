ce7e93702dfaf08fd9be196920daa36c6c0d937c
Kevin Boos <kevinaboos@gmail.com>
2022-05-05 14:47:18 -0500

    [no ci] Use Theseus-specific compiler options to build `wasmtime-runtime` `helpers.c` file.
    Still need to actually perform partial linking of `libwastime-helpers.a`
    with the emitted wasmtime-runtime object file

--- crates/runtime/build.rs
+++ crates/runtime/build.rs
@@ -2,12 +2,30 @@ use std::env;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/helpers.c");
-    cc::Build::new()
-        .warnings(true)
+    let mut build = cc::Build::new();
+    build.warnings(true)
         .define(
             &format!("CFG_TARGET_OS_{}", env::var("CARGO_CFG_TARGET_OS").unwrap()),
             None,
         )
-        .file("src/helpers.c")
-        .compile("wasmtime-helpers");
+        .file("src/helpers.c");
+    
+        if env::var("CARGO_CFG_TARGET_OS").unwrap().contains("theseus") {
+            build
+                .use_plt(false)
+                .pic(false)
+                // Theseus doesn't yet support the GOT (Global Offset Table),
+                // so we disable usage of the GOT by forcing no PLT and PIC.
+                // In Rust, this is accomplished using `-C relocation-model=static`.
+                .flag("-fno-plt")
+                .flag("-fno-pic")
+                // Theseus currently uses the large code model.
+                .flag("-mcmodel=large") 
+                // Disable stack smashing protection
+                .flag("-fno-stack-protector");
+
+                // eprintln!("Theseus build: {:?}\n{:?}", build, build.get_compiler());
+        }
+
+    build.compile("wasmtime-helpers");
 }
